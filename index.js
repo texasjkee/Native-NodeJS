b774f025d15cb58ceee77f68d9672e21bacdeabf
texasjkee <gt.gluck@gmail.com>
2023-02-19 11:31:58 +0200

    add typescript into the project

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-const express = require('express');
-const app = express();
-const PORT = 3033;
-
-app.get('/', (req, res) => {
-  res.send('texasjkee')
-})
-
-app.get('/users', (req, res) => {
-  res.send('hello friends')
-})
-
-app.post('/users', (req, res) => {
-  res.send('Goodbye friends')
-})
-
-app.listen(PORT, () => {
-  console.log(`Starting app at port ${PORT} `)
-})
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+var PORT = 3033;
+app.get('/', function (req, res) {
+    res.send('texasjkee');
+});
+app.get('/users', function (req, res) {
+    res.send('hello friends');
+});
+app.post('/users', function (req, res) {
+    res.send('Goodbye friends');
+});
+app.listen(PORT, function () {
+    console.log("Starting app at port ".concat(PORT, " "));
+});

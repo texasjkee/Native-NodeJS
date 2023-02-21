9c7ca209fe6e65e5c8db20fce3f8c9916ef63498
texasjkee <gt.gluck@gmail.com>
2023-02-21 15:32:36 +0200

    fix: http_headers error
      Fix ERR_HTTP_HEADERS_SENT: Cannot set header he client. I just removed res.sendStatus method in get response

---
+++ dist/src/index.js
@@ -0,0 +1,79 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const express_1 = __importDefault(require("express"));
+exports.app = (0, express_1.default)();
+const PORT = 3033;
+const HTTP_STATUS = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+    BAD_REQUEST_400: 400,
+    NOT_FOUND_404: 404
+};
+const jsonBodyMiddleware = express_1.default.json();
+exports.app.use(jsonBodyMiddleware);
+const db = {
+    users: [
+        { id: 1, title: 'example#1' },
+        { id: 2, title: 'example#2' },
+        { id: 3, title: 'example#3' },
+        { id: 4, title: 'example#4' },
+        { id: 5, title: 'example#5' },
+    ]
+};
+exports.app.get('/', (req, res) => {
+    res.sendStatus(300);
+});
+exports.app.get('/user/:id', (req, res) => {
+    const foundUser = db.users.find(el => el.id === +req.params.id);
+    if (!foundUser) {
+        res.sendStatus(HTTP_STATUS.NOT_FOUND_404);
+        return;
+    }
+    res.json(foundUser);
+});
+exports.app.get('/users', (req, res) => {
+    let foundUsers = db.users;
+    if (req.query.title) {
+        foundUsers = foundUsers.filter(t => t.title.indexOf(req.query.title) > -1);
+    }
+    res.json(foundUsers);
+});
+exports.app.post('/user', (req, res) => {
+    if (!req.body.title) {
+        res.sendStatus(400);
+        return;
+    }
+    const createdUser = {
+        id: +(new Date()),
+        title: req.body.title
+    };
+    db.users.push(createdUser);
+    res
+        .sendStatus(HTTP_STATUS.CREATED_201)
+        .json(createdUser);
+});
+exports.app.delete('/user/:id', (req, res) => {
+    const deleteUser = db.users = db.users.filter(el => el.id !== +req.params.id);
+    res
+        .sendStatus(HTTP_STATUS.NO_CONTENT_204)
+        .json(deleteUser);
+});
+exports.app.put('/user/:id', (req, res) => {
+    const foundUser = db.users.find(el => el.id === +req.params.id);
+    if (!foundUser) {
+        res.sendStatus(HTTP_STATUS.NOT_FOUND_404);
+        return;
+    }
+    foundUser.title = req.body.title;
+    res
+        .sendStatus(HTTP_STATUS.NO_CONTENT_204)
+        .json(foundUser);
+});
+exports.app.listen(PORT, () => {
+    console.log(`Starting app at port ${PORT} `);
+});
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-const app = express();
+export const app = express();
 const PORT = 3033;
 
 const HTTP_STATUS = {
@@ -17,38 +17,34 @@ const jsonBodyMiddleware = express.json()
 app.use(jsonBodyMiddleware);
 
 const db = {
- tests: [
-    {id: 1, title: 'front-end'},
-    {id: 2, title: 'devops'},
-    {id: 3, title: 'back-end'},
-    {id: 4, title: 'testing'},
-    {id: 5, title: 'gaming'},
+ users: [
+    {id: 1, title: 'example#1'},
+    {id: 2, title: 'example#2'},
+    {id: 3, title: 'example#3'},
+    {id: 4, title: 'example#4'},
+    {id: 5, title: 'example#5'},
   ] 
 }
 
 app.get('/', (req, res) => {
-  res.send('texasjkee')
+  res.sendStatus(300)
 })
 
-app.get('/test/:id', (req, res) => {
-  const foundTest = db.tests.find(el => el.id === +req.params.id)
-  if(!foundTest) {
+app.get('/user/:id', (req, res) => {
+  const foundUser = db.users.find(el => el.id === +req.params.id)
+  if(!foundUser) {
     res.sendStatus(HTTP_STATUS.NOT_FOUND_404);
     return;
   }
-  res
-    .sendStatus(HTTP_STATUS.OK_200)
-    .json(foundTest)
+  res.json(foundUser)
 })
 
-app.get('/tests', (req, res) => {
-  let foundTests = db.tests;
+app.get('/users', (req, res) => {
+  let foundUsers = db.users;
   if(req.query.title) {
-     foundTests = foundTests.filter(t => t.title.indexOf(req.query.title as string) > -1);
+     foundUsers = foundUsers.filter(t => t.title.indexOf(req.query.title as string) > -1);
   }
-  res
-    .sendStatus(HTTP_STATUS.OK_200)
-    .json(foundTests)
+  res.json(foundUsers)
 })
 
 app.post('/user', (req, res) => {
@@ -56,33 +52,33 @@ app.post('/user', (req, res) => {
     res.sendStatus(400);
     return
    }
-  const createdTest = {
+  const createdUser = {
     id: +(new Date()),
     title: req.body.title
   }
-  db.tests.push(createdTest)
+  db.users.push(createdUser)
   res
     .sendStatus(HTTP_STATUS.CREATED_201)
-    .json(createdTest)
+    .json(createdUser)
 })
 
-app.delete('/test/:id', (req, res) => {
-  const deleteTest = db.tests = db.tests.filter(el => el.id !== +req.params.id);
+app.delete('/user/:id', (req, res) => {
+  const deleteUser = db.users = db.users.filter(el => el.id !== +req.params.id);
   res
     .sendStatus(HTTP_STATUS.NO_CONTENT_204)
-    .json(deleteTest);
+    .json(deleteUser);
 })
 
-app.put('/test/:id', (req, res) => {
-  const foundTest = db.tests.find(el => el.id === +req.params.id)
-  if(!foundTest) {
+app.put('/user/:id', (req, res) => {
+  const foundUser = db.users.find(el => el.id === +req.params.id)
+  if(!foundUser) {
     res.sendStatus(HTTP_STATUS.NOT_FOUND_404);
     return;
   }
-  foundTest.title = req.body.title; 
+  foundUser.title = req.body.title; 
   res
     .sendStatus(HTTP_STATUS.NO_CONTENT_204)
-    .json(foundTest)
+    .json(foundUser)
 })
 
 app.listen(PORT, () => {
